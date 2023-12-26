83f529b09de8538b8ee8fea3c156be8887a089dd
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-12-26 18:20:27 -0300

    feat: add migrations

--- src/user/interfaces/user.entity.ts
+++ src/user/interfaces/user.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({ name: 'user' })
 export class UserEntity {
@@ -14,4 +20,12 @@ export class UserEntity {
   cpf: string;
   @Column({ name: 'password', nullable: false })
   password: string;
+  @Column({ name: 'type_user', nullable: false })
+  typeUser: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
