1ed6c1c8cd98953f27c8464bd7fd74b47ab64c8e
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-12-28 11:13:10 -0300

    feat: add authentication role

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -9,11 +9,14 @@ import {
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/createAddress.dto';
 import { AddressEntity } from './entities/address.entity';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/user-type.enum';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
+  @Roles(UserType.User)
   @UsePipes(ValidationPipe)
   @Post('/:userId')
   async createUser(
