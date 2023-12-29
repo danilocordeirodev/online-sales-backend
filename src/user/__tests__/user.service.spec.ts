b1fbba3d46687ead88c3181e0a93b45c5c44ed8b
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-12-29 17:51:11 -0300

    feat: include user test

--- src/user/__tests__/user.service.spec.ts
+++ src/user/__tests__/user.service.spec.ts
@@ -1,19 +1,34 @@
-import { beforeEach } from 'node:test';
 import { UserService } from '../user.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserEntity } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: Repository<UserEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue({}),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(userRepository).toBeDefined();
   });
 });
