ab56937aaf4ca2c19677998441820f986d36140c
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-12-30 10:49:05 -0300

    feat: create unit tests for state and city

---
+++ src/city/__tests__/city.service.spec.ts
@@ -0,0 +1,58 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { cityMock } from '../__mocks__/city.mock';
+import { CityService } from '../city.service';
+import { CityEntity } from '../entities/city.entity';
+import { CacheService } from '../../cache/cache.service';
+
+describe('CityService', () => {
+  let service: CityService;
+  let cityRepository: Repository<CityEntity>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CityService,
+        {
+          provide: CacheService,
+          useValue: {
+            getCache: jest.fn().mockResolvedValue([cityMock]),
+          },
+        },
+        {
+          provide: getRepositoryToken(CityEntity),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(cityMock),
+            find: jest.fn().mockResolvedValue([cityMock]),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<CityService>(CityService);
+    cityRepository = module.get<Repository<CityEntity>>(
+      getRepositoryToken(CityEntity),
+    );
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(cityRepository).toBeDefined();
+  });
+
+  it('should return city - findOne', async () => {
+    const city = await service.getCityById(cityMock.id);
+    expect(city).toEqual(cityMock);
+  });
+
+  it('should return error - findOne', async () => {
+    jest.spyOn(cityRepository, 'findOne').mockResolvedValue(undefined);
+    expect(service.getCityById(cityMock.id)).rejects.toThrow();
+  });
+
+  it('should return cities by state id', async () => {
+    const city = await service.getAllCitiesByStateId(cityMock.stateId);
+    expect(city).toEqual(cityMock);
+  });
+});
